22d421eb9904c9471f00bf8a9238fc228cfba31c
Rendy Arya Kemal <renrror@gmail.com>
2023-11-26 09:48:03 +0700

    change admin title

--- absen/plugins.py
+++ absen/plugins.py
@@ -4,7 +4,7 @@ from flask_login import LoginManager, current_user as original_current_user
 
 from absen.models import User, db
 
-admin = Admin(name="Absensi", template_mode="bootstrap4")
+admin = Admin(name="SiAbsen", template_mode="bootstrap4")
 migrate = Migrate(db=db)
 login_manager = LoginManager()
 current_user: User = original_current_user  # type: ignore
