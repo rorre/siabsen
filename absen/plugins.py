346ba28ca136bdac6dc2d1e0e0785b17fab3494d
Rendy Arya Kemal <renrror@gmail.com>
2023-12-09 14:41:06 +0700

    Add simple navigation to admin home

--- absen/plugins.py
+++ absen/plugins.py
@@ -1,10 +1,14 @@
-from flask_admin import Admin
+from flask_admin import Admin, AdminIndexView
 from flask_migrate import Migrate
 from flask_login import LoginManager, current_user as original_current_user
 
 from absen.models import User, db
 
-admin = Admin(name="SiAbsen", template_mode="bootstrap4")
+admin = Admin(
+    name="SiAbsen",
+    template_mode="bootstrap4",
+    index_view=AdminIndexView(name="Home", template="admin/home.html", url="/admin"),
+)
 migrate = Migrate(db=db)
 login_manager = LoginManager()
 current_user: User = original_current_user  # type: ignore
