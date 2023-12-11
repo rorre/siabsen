3ae24643b58fe2150db8c68f1b5c8f42a2716717
Rendy Arya Kemal <renrror@gmail.com>
2023-12-11 10:57:34 +0700

    Wait for migration before startup

--- startup.sh
+++ startup.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+echo "Waiting for postgres and migration..."
+while true
+do
+    flask db upgrade
+    if [ $? = 0 ]; then
+        break
+    fi
 
-flask db upgrade
+    echo -n "."
+done
+
 gunicorn -w 3 -t 60 -b 0.0.0.0:5000 app:app
